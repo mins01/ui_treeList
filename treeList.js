1e6b5d15dee3084af4c94149e831a0657363b47f
mins01 <mins01.lycos.co.kr@gmail.com>
2021-03-11 15:04:35 +0900

    hover 부분 명칭 변경, 동작 변경 hover-with-child 명칭 추가 treeList.js 로 접기 펼치기 이벤트 자동화 theme-0 추가

---
+++ treeList.js
@@ -0,0 +1,67 @@
+var treeList = (function treeList(){
+	let onclick = function(evt){
+		let target = evt.target;
+		if(target.classList.contains('treeList-show-stem')){
+			let node = target.closest('.treeList .treeList-branch , .treeListSimple li'); if(!node){ return; }
+			treeList.show(node);
+		}else if(target.classList.contains('treeList-hide-stem')){
+			let node = target.closest('.treeList .treeList-branch , .treeListSimple li'); if(!node){ return; }
+			treeList.hide(node);
+		}else if(target.classList.contains('treeList-toggle-stem')){
+			let node = target.closest('.treeList .treeList-branch , .treeListSimple li'); if(!node){ return; }
+			treeList.toggle(node);
+		}else if(target.classList.contains('treeList-show-parent-stem')){
+			let node = target.closest('.treeList .treeList-branch , .treeListSimple li').parentNode; if(!node){ return; }
+			node = node.closest('.treeList .treeList-branch , .treeListSimple li'); if(!node){ return; }
+			treeList.show(node);
+		}else if(target.classList.contains('treeList-hide-parent-stem')){
+			let node = target.closest('.treeList .treeList-branch , .treeListSimple li').parentNode; if(!node){ return; }
+			node = node.closest('.treeList .treeList-branch , .treeListSimple li'); if(!node){ return; }
+			treeList.hide(node);
+		}else if(target.classList.contains('treeList-toggle-parent-stem')){
+			let node = target.closest('.treeList .treeList-branch , .treeListSimple li').parentNode; if(!node){ return; }
+			node = node.closest('.treeList .treeList-branch , .treeListSimple li'); if(!node){ return; }
+			treeList.toggle(node);
+		}
+	}
+	let addedEvent = false;
+	let treeList = {
+		"debug":false,
+		"init":function(){
+			// this.addEvent();
+		},
+		"addEvent":function(){
+			if(addedEvent){
+				if(this.debug) console.log("treeList: already addedEvent");
+				return false;;
+			}
+			document.addEventListener('click',onclick);
+			addedEvent = true;
+			if(this.debug) console.log("treeList: treeList.addEvent()");
+			return true;
+		},
+		"removeEvent":function(){
+			if(!addedEvent){
+				if(this.debug) console.log("treeList: already !addedEvent");
+				return false;;
+			}
+			document.removeEventListener('click',onclick);
+			addedEvent = false;
+			if(this.debug) console.log("treeList: treeList.removeEvent()");
+			return true;
+		},
+		"toggle":function(node){
+			node.classList.toggle('treeList-hidden-stem')
+			if(this.debug) console.log("treeList: treeList.toggle(node)",node);
+		},
+		"hide":function(node){
+			node.classList.add('treeList-hidden-stem')
+			if(this.debug) console.log("treeList: treeList.hide(node)",node);
+		},
+		"show":function(node){
+			node.classList.remove('treeList-hidden-stem')
+			if(this.debug) console.log("treeList: treeList.show(node)",node);
+		},
+	}
+	return treeList;
+})()
